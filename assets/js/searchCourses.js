5d5dee65ba141b2fda621d1beb6a619966728963
Mina <mina.fawzy02@eng-st.cu.edu.eg>
2022-12-21 13:39:31 +0200

    Socket Optimized

--- assets/js/searchCourses.js
+++ assets/js/searchCourses.js
@@ -5,26 +5,53 @@ let searchInput = document.getElementById("searchInput");
 let start = 1;
 
 socket.on("coursesSearched", function (coursesSearched) {
-    searchDiv.innerHTML = "";
-    coursesSearched.forEach(function (val) {
-        searchDiv.innerHTML += `
-        
-        <div class="p-2 bd-highlight">
-                    <div class="flex-item">
+    let i = 0;
+    console.log(coursesSearched);
+    for (let k = 0; k < coursesSearched.length; k++) {
+        console.log(i);
+        if (coursesSearched.length < searchDiv.children.length && coursesSearched[k].CourseID.toString() != searchDiv.children[i].classList[2].toString()) {
+            // change image as well when it is here.
+            searchDiv.removeChild(searchDiv.children[i]);
+            k--;
+            i--;
+        }
+        else if (!searchDiv.children[i] || (coursesSearched.length > searchDiv.children.length && coursesSearched[k].CourseID.toString() != searchDiv.children[i].classList[2].toString())) {
+
+            var newElement = new DOMParser().parseFromString(`
+            
+            <div class="p-2 bd-highlight">
+                <div class="flex-item">
                     <div class="flex-shrink-0">
                         <img src="..." alt="...">
                     </div>
                     <div>
-                        <a href="/courseinfo/` + val.CourseID + `>"><h5>` + val.Prefix + `></h5></a>
+                        <a href="/courseinfo/` + coursesSearched[k].CourseID + `>"><h5>` + coursesSearched[k].Prefix + `></h5></a>
                     </div>
                     <div class="flex-grow-1 ms-3">
-                        <h6>` + val.Course_Name + `</h6>
-                    </div>
+                        <h6>` + coursesSearched[k].Course_Name + `</h6>
                     </div>
                 </div>
-        
-        `;
-    });
+            </div>
+            
+            `, "text/html").body.children[0];
+
+            console.log(newElement);
+
+            newElement.classList.add(coursesSearched[k].CourseID);
+            if (searchDiv.children[i]) {
+                searchDiv.insertBefore(newElement, searchDiv.children[i]);
+            }
+            else {
+                searchDiv.appendChild(newElement);
+            }
+
+        }
+        i++;
+    };
+    let j = i;
+    while (searchDiv.children.length != i) {
+        searchDiv.removeChild(searchDiv.children[j]);
+    }
 });
 window.onload = function (ev) {
     searchInput.addEventListener("input", function (ev1) {
