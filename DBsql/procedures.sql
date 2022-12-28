0d4804ba645110eb175379c7debd6c9acef19b7d
Mina <mina.fawzy02@eng-st.cu.edu.eg>
2022-12-29 01:24:31 +0200

    Procedures updated

--- DBsql/procedures.sql
+++ DBsql/procedures.sql
@@ -1,7 +1,7 @@
 DELIMITER $$ 
 CREATE PROCEDURE GetNameOfInstructor( IN CourseID varchar(20)) 
 BEGIN 
-SELECT F_Name , L_Name from Users , teaches where teaches.CourseID  = CourseID AND teaches.InstructorID = Users.loginID;
+SELECT F_Name , L_Name, LoginID from Users , teaches where teaches.CourseID  = CourseID AND teaches.InstructorID = Users.loginID;
 END 
 $$
 delimiter ;
