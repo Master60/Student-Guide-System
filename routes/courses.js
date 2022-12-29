83bbabedbd8a40d1d533b3a368d0dfb5b531355e
Mina <mina.fawzy02@eng-st.cu.edu.eg>
2022-12-29 05:05:43 +0200

    Add Ticket Modified

--- routes/courses.js
+++ routes/courses.js
@@ -6,13 +6,12 @@ var express = require("express"),
 router.get("/", function (req, res, next) {
     req.session.refe = "/courses";
     isAuthorized(req, res, function () {
-        connection.query("select CollegeID from users where LoginID = " + req.session.identity + ";", function (err, result) {
-            console.log(result[0].CollegeID); 
-            connection.query("CALL GetcoursesInCollege(?)" , [result[0].CollegeID], function (err, CourseList) {
-            res.render("Nuno Theme Starter Files/courses.ejs", {
-                courses: CourseList[0]
+        connection.query("select CollegeID from users where LoginID = '" + req.session.identity + "'", function (err, result) {
+            connection.query("CALL GetcoursesInCollege(?)", [result[0].CollegeID], function (err, CourseList) {
+                res.render("Nuno Theme Starter Files/courses.ejs", {
+                    courses: CourseList[0]
+                });
             });
-        });
         });
     });
 });
@@ -26,27 +25,31 @@ router.get("/:course", function (req, res, next) {
                     connection.query("call GetNameOfInstructor(?)", req.params.course, function (err, Names) {
                         connection.query("CALL GetContactsOf(?)", [Names[0][0].LoginID], function (err, contact) {
                             connection.query("CALL GetElearningMaterial(?)", [req.params.course], function (err, Elearning) {
-    
-                                let contacttemp = undefined;
-                                for (let i = 0; i < contact[0].length; i++ ) {
-                                    if(contact[0][i].ContactType == "email") {
-                                        contacttemp = contact[0][i].Contact;
-                                        break;
-                                    }
-                                }
+                                connection.query("SELECT COUNT(*) FROM s_takes_c WHERE CourseID='" +
+                                    req.params.course + "' AND StudentID='" + req.session.identity + "'", function (errr, follows) {
 
-                                if (!contacttemp) {
-                                    contacttemp = contact[0][0].Contact;
-                                }
+                                        let contacttemp = undefined;
+                                        for (let i = 0; i < contact[0].length; i++) {
+                                            if (contact[0][i].ContactType == "email") {
+                                                contacttemp = contact[0][i].Contact;
+                                                break;
+                                            }
+                                        }
 
-                                res.render("Nuno Theme Starter Files/course info.ejs", {
-                                    course: result[0][0],
-                                    Semester: Semesters[0],
-                                    InstructorName: Names[0],
-                                    contactObj: contacttemp,
-                                    LearningMaterial : Elearning[0]
-                                });
-                        
+                                        if (!contacttemp) {
+                                            contacttemp = contact[0][0].Contact;
+                                        }
+
+                                        res.render("Nuno Theme Starter Files/course info.ejs", {
+                                            course: result[0][0],
+                                            Semester: Semesters[0],
+                                            InstructorName: Names[0],
+                                            contactObj: contacttemp,
+                                            LearningMaterial: Elearning[0],
+                                            follow: follows[0]["COUNT(*)"]
+                                        });
+
+                                    });
                             });
                         });
                     });
